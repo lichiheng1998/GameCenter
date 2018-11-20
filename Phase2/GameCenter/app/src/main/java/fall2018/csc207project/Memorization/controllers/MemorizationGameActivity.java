a1e9904b7d4253f71764c1759a62f5d71758b15f
Coco <wenjie.lu@mail.utoronto.ca>
2018-11-20 00:55:35 -0500

    add a general structure of memorization game starting activity

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/controllers/MemorizationGameActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/controllers/MemorizationGameActivity.java
@@ -3,13 +3,37 @@ package fall2018.csc207project.Memorization.controllers;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import fall2018.csc207project.SlidingTile.Views.GestureDetectGridView;
+import fall2018.csc207project.R;
 
 public class MemorizationGameActivity extends AppCompatActivity {
-    private GestureDetectGridView gridView;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo_game_starting);
+        addNewGameButtonListener();
+        addMyTopScoresButtonListener();
+        addTopPlayersButtonListener();
+    }
+
+
+    private void addNewGameButtonListener(){
+        Button newGameButton = findViewById(R.id.newMemogame);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToBoardsSize();
+            }
+        });
+    }
+
+    private void switchToBoardsSize() {
+    }
+
+    private void addMyTopScoresButtonListener(){}
+
+    private void addTopPlayersButtonListener() {
     }
 }
