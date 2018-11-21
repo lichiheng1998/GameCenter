3855bec40f49986cfd5a1e7f7a3c9096c0c5bdf8
Wenhaoooooo <zhangwh41@gmail.com>
2018-11-21 10:17:11 -0500

    merge the scoreboard initial design with existing application

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileScore.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileScore.java
@@ -4,4 +4,14 @@ import fall2018.csc207project.models.Score;
 
 public class TileScore extends Score{
 
+
+    public int complexity;
+    public String game = "Slidetail";
+    public String user;
+    public int value;
+
+    TileScore(int complexity){
+        this.complexity = complexity;
+    }
+
 }
