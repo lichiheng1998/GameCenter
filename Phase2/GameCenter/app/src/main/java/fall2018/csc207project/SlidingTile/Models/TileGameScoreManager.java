372a26383af47e6f06c9de13732edddd414ad234
Wenhaoooooo <zhangwh41@gmail.com>
2018-11-21 15:51:37 -0500

    merge TileGameScoreManage with existing app

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileGameScoreManager.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileGameScoreManager.java
@@ -1,15 +1,42 @@
 package fall2018.csc207project.SlidingTile.Models;
 
-public class TileGameScoreManager {
+import java.util.ArrayList;
+
+import fall2018.csc207project.models.ScoreDataStream;
+import fall2018.csc207project.models.ScoreManager;
+
+public class TileGameScoreManager extends ScoreManager {
 
     private int complexity;
-    private String currrentUser;
+    private String game = "SlidingTail";
 
-    TileGameScoreManager(int complexity, String currrentUser){
+    TileGameScoreManager(String userName, int complexity, ScoreDataStream dataStream, String user,
+                         ScoreCalculator<T> calculator){
+        super(dataStream, "SlidingTile", user, calculator);
         this.complexity = complexity;
-        this.currrentUser = currrentUser;
     }
-    public getHighestScore(){
-        this.getScorebyUser(this.currrentUser);
+    public TileScore getHighestbyUserLevel3 (String userName){
+        return null;
+    }
+    public TileScore getHighestbyUserLevel4 (String userName){
+        return null;
+    }
+    public TileScore getHighestbyUserLevel5 (String userName){
+        return null;
+    }
+    public TileScore getHighestbyUserLevel6 (String userName){
+        return null;
+    }
+    public ArrayList<TileScore> getHighestTenbyLevel3 (String userName){
+        return null;
+    }
+    public ArrayList<TileScore> getHighestTenbyLevel4 (String userName){
+        return null;
+    }
+    public ArrayList<TileScore> getHighestTenbyLevel5 (String userName){
+        return null;
+    }
+    public ArrayList<TileScore> getHighestTenbyLevel6 (String userName){
+        return null;
     }
 }
