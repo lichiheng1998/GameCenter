4acc0c650bb729ff6f304aeffb7b4b3a789f9916
Coco <wenjie.lu@mail.utoronto.ca>
2018-11-24 16:40:56 -0500

    add new boolean parameter level when initialize a new MemoManger change algorithm of method updateActiveTiles according to level

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/Models/MemoManager.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/Models/MemoManager.java
@@ -27,18 +27,25 @@ public class MemoManager implements Iterable<MemoTile>, Serializable{
     private List<MemoTile> sequenceOrder;
 
     /**
+     * hard or normal game level
+     */
+    private boolean level;
+
+    /**
      * initialize a new MemoManger
      * @param width width of the game board
      * @param height height of the game board
      */
-    public MemoManager(int width, int height){
+    public MemoManager(int width, int height, boolean level){
         this.width = width;
         this.height = height;
+        this.level = level;
         // Set the current complexity to be 1.
         curComplexity = 10;
         sequenceOrder = new ArrayList<>();
         // Update the active tiles.
         updateActiveTiles();
+
     }
 
     /**
@@ -69,16 +76,29 @@ public class MemoManager implements Iterable<MemoTile>, Serializable{
 
     /**
      * regenerate random active memoTiles to update sequenceOrder
+     * different generate method based on value of level
      */
     public void updateActiveTiles(){
-        sequenceOrder.clear();
         int randomID;
         int bound = getSize();
         // Generate some Tiles with random ids.
-        for (int count = 0; count < curComplexity; count++) {
+        if(level){
+            sequenceOrder.clear();
+            for (int count = 0; count < curComplexity; count++) {
+                randomID = ThreadLocalRandom.current().nextInt(bound);
+                int fake = ThreadLocalRandom.current().nextInt(10);
+                if(fake > 2){
+                    sequenceOrder.add(new MemoTile(randomID, MemoTile.TYPEACTIVE));
+                } else {
+                    sequenceOrder.add(new MemoTile(randomID, MemoTile.TYPEFAKE));
+                }
+            }
+        } else {
             randomID = ThreadLocalRandom.current().nextInt(bound);
             sequenceOrder.add(new MemoTile(randomID, MemoTile.TYPEACTIVE));
         }
+
         curComplexity += 1;
     }
+
 }
