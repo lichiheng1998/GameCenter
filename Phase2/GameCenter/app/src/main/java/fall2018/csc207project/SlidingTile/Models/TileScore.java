3789251f91d7561bc3d243b10e7005e07305ab82
Arthur <arthurqin123@hotmail.com>
2018-11-24 17:13:07 -0500

    edit methods

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileScore.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileScore.java
@@ -15,31 +15,32 @@ public class TileScore extends Score{
     /**
      * name of current game
      */
-    public String game = "Slidetail";
+    public final String game = "SlidingTilesGame";
 
     /**
      * name of the current player
      */
     public String user;
 
-    /**
-     * score of the specific game for a specific player in a specific complexity
-     */
-    public int value;
+//    /**
+//     * score of the specific game for a specific player in a specific complexity
+//     */
+//    public int value;
     public int undoSteps;
     public int moveSteps;
 
-    public TileScore(int complexity, String userName, int score){
-        this.complexity = complexity;
+//    public TileScore(int complexity, String userName, int score){
+//        this.complexity = complexity;
+//        this.user = userName;
+//        this.value = score;
+//    }
+    public TileScore(String userName, int complexity,  int undoSteps, int moveSteps){
         this.user = userName;
-        this.value = score;
-    }
-    public TileScore(int complexity, String userName, int undoSteps, int moveSteps){
         this.complexity = complexity;
-        this.user = userName;
         this.undoSteps = undoSteps;
         this.moveSteps = moveSteps;
     }
+
 //
 //    public boolean compareTo(TileScore score) {
 //        return value >= score.value;
