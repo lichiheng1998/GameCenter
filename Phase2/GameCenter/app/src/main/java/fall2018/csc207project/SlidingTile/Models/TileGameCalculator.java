f19f0a9339fb2d24e80c644ce75269e9feac2895
Arthur <arthurqin123@hotmail.com>
2018-11-24 14:42:06 -0500

    merge the class SlidingTileScore to TileGameCalculator

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileGameCalculator.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileGameCalculator.java
@@ -7,4 +7,22 @@ public class TileGameCalculator implements ScoreCalculator<TileScore> {
     public int calculate(TileScore scoreInfo) {
         return 1000;
     }
+
+    public int calculateScore(int complexity, int totalMoveSteps, int totalUndoSteps) {
+        int maxScore = 1000;
+        int finalScore = 0;
+        if (complexity == 3) {
+            finalScore = maxScore - 7 * totalMoveSteps - totalUndoSteps;
+        } else if (complexity == 4) {
+            finalScore = maxScore - 2 * totalMoveSteps - totalUndoSteps;
+        } else if (complexity == 5) {
+            finalScore = maxScore - totalMoveSteps - totalUndoSteps;
+        }
+        if (finalScore > 0){
+            return finalScore;
+        }
+        else {
+            return 0;
+        }
+    }
 }
