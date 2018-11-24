e2285d52adc362877c959099cd2c3741ad43dcbb
Chiheng Li <lichiheng1998@gmail.com>
2018-11-24 18:50:16 -0500

    totally remove the GestureDectetView for sliding tile game.

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Controllers/GameActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Controllers/GameActivity.java
@@ -6,14 +6,14 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.NumberPicker;
 import android.widget.Toast;
-import java.util.Collections;
+
 import java.util.List;
 
 import fall2018.csc207project.SlidingTile.Models.BoardManager;
 import fall2018.csc207project.SlidingTile.Views.BoardGameView;
-import fall2018.csc207project.SlidingTile.Views.GestureDetectGridView;
 import fall2018.csc207project.R;
 import fall2018.csc207project.SlidingTile.Views.NumberPickerDialog;
 
@@ -32,7 +32,7 @@ public class GameActivity extends AppCompatActivity implements BoardGameView{
     private List<Button> tileButtons;
 
     // Grid View and calculated column height and width based on device size
-    private GestureDetectGridView gridView;
+    private GridView gridView;
     private BoardGamePresenter presenter;
 
     /**
@@ -49,7 +49,8 @@ public class GameActivity extends AppCompatActivity implements BoardGameView{
         super.onCreate(savedInstanceState);
         this.presenter = new BoardGamePresenter(this, getApplicationContext());
         BoardManager boardManager = (BoardManager) getIntent().getSerializableExtra("save");
-        this.tileButtons = boardManager.getButtonList(this);
+        presenter.setBoardManager(boardManager);
+        this.tileButtons = presenter.getButtonList(this);
         setContentView(R.layout.activity_main);
         undoText = findViewById(R.id.StepsToUndo);
         setupGridView(boardManager);
@@ -61,10 +62,8 @@ public class GameActivity extends AppCompatActivity implements BoardGameView{
      * Setup the customized grid view and get the height and weight of the tiles.
      */
     private void setupGridView(final BoardManager boardManager){
-        presenter.setBoardManager(boardManager);
         // Add View to activity
         gridView = findViewById(R.id.grid);
-        gridView.setBoardGamePresenter(presenter);
         gridView.setNumColumns(boardManager.getComplexity());
         // Observer sets up desired dimensions as well as calls our display function
         gridView.getViewTreeObserver().addOnGlobalLayoutListener(
@@ -83,6 +82,12 @@ public class GameActivity extends AppCompatActivity implements BoardGameView{
                 });
     }
 
+    /**
+     * Setup grid view,
+     */
+    private void setupGridViewLisnter(){
+
+    }
     /**
      * Active the listener for the step picker edit text.
      */
@@ -119,8 +124,9 @@ public class GameActivity extends AppCompatActivity implements BoardGameView{
      * Swap the buttons of position pos1 and pos2.
      */
     public void swapButtons(int pos1, int pos2){
-        Collections.swap(tileButtons, pos1, pos2);
-        display();
+        String text = tileButtons.get(pos1).getText().toString();
+        tileButtons.get(pos1).setText(tileButtons.get(pos2).getText());
+        tileButtons.get(pos2).setText(text);
     }
 
     /**
