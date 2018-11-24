6331a6c1418ffc77c19d5e53aab8533179e13b37
Wenhaoooooo <zhangwh41@gmail.com>
2018-11-24 18:15:44 -0500

    ScoreBoardActivity class

---
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Controllers/ScoreBoardActivity.java
@@ -0,0 +1,25 @@
+package fall2018.csc207project.SlidingTile.Controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScoreBoardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+
+
+    }
+
+    public void add3Listener (){
+
+    }
+
+    public void add4Listener() {
+
+    }
+
+    public void add5Listener(){
+
+    }
+}
