afc383e2c7141fc5c61371abe9caa7ac0fd89c68
Coco <wenjie.lu@mail.utoronto.ca>
2018-11-24 17:29:40 -0500

    add switch listener for hard mode switch

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/Controllers/MemoComplxActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/Controllers/MemoComplxActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import fall2018.csc207project.Memorization.Models.MemoManager;
 import fall2018.csc207project.R;
@@ -13,6 +15,7 @@ import fall2018.csc207project.R;
 public class MemoComplxActivity extends AppCompatActivity {
     private int width;
     private int height;
+    private boolean level=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MemoComplxActivity extends AppCompatActivity {
         addL3ButtonListener();
         addL4ButtonListener();
         addL5ButtonListener();
+        hardModeSwitchListener();
 
     }
 
@@ -61,9 +65,23 @@ public class MemoComplxActivity extends AppCompatActivity {
 
     }
 
+    private void hardModeSwitchListener(){
+        Switch hardModeSwitch = findViewById(R.id.hardmode);
+        hardModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    level = true;
+                } else {
+                    level = false;
+                }
+            }
+        });
+    }
+
     private void switchToMemoGame() {
         Intent tmp = new Intent(this, MemoGameActivity.class);
-        tmp.putExtra("save", new MemoManager(width, height));
+        tmp.putExtra("save", new MemoManager(width, height, level));
         startActivity(tmp);
         finish();
     }
