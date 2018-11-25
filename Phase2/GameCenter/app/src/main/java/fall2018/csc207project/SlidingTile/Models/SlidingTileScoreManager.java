50410a27226351fd11472619b675853a68a507ed
Arthur <arthurqin123@hotmail.com>
2018-11-25 18:07:44 -0500

    add getTopTenScoreBoard() method

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/SlidingTileScoreManager.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/SlidingTileScoreManager.java
@@ -1,6 +1,9 @@
 package fall2018.csc207project.SlidingTile.Models;
 
+import android.content.Context;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import fall2018.csc207project.models.ScoreCalculator;
@@ -13,13 +16,29 @@ public class SlidingTileScoreManager extends ScoreManager<TileScore>{
         super(dataStream, "SlidingTilesGame", user, calculator);
     }
 
-    public List<TileScore> filter(ArrayList<TileScore> filterList, int filterLevel){
-        List res = new ArrayList();
-        for (TileScore score: filterList){
-            if (score.complexity == filterLevel ) {
-                res.add(score);
+    /**
+     * Get the top ten TileScores by giving a context and a complexity
+     *
+     * @param context the context of the app
+     * @param complexity the complexity the user chooses
+     * @return the top ten TileScores
+     */
+    public List<TileScore> getTopTenScoreBoard(Context context, int complexity){
+        List<TileScore> tileScores = getScoresOfGame(context);
+        List<TileScore> scoreBoardScores = new ArrayList<>();
+        for (TileScore tileScore : tileScores) {
+            if (tileScore.complexity == complexity) {
+                scoreBoardScores.add(tileScore);
             }
         }
-        return res;
+
+        Collections.sort(scoreBoardScores);
+
+        List<TileScore> topTenScoreBoard = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            topTenScoreBoard.add(scoreBoardScores.get(i));
+        }
+
+        return topTenScoreBoard;
     }
 }
