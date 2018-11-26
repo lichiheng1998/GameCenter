0dd8a4998ededb7cd7913dacb490e89562339fa5
Arthur <arthurqin123@hotmail.com>
2018-11-25 20:23:23 -0500

    add javadoc and a getter

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/models/ScoreManager.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/models/ScoreManager.java
@@ -1,20 +1,44 @@
 package fall2018.csc207project.models;
 
 import android.content.Context;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-@SuppressWarnings("unchecked")
+
 /**
  * The class that is used to calculate the score, store and retrieve the score.
  */
+@SuppressWarnings("unchecked")
 public class ScoreManager <T extends Score>{
+
+    /**
+     * The current game that is now operating.
+     */
     private String currentGame;
+
+    /**
+     * The current user that is now operating.
+     */
     private String currentUser;
+
+    /**
+     * the score data needed for operating.
+     */
     private ScoreDataStream dataStream;
+
+    /**
+     * the type of score calculator needed for calculate the current game.
+     */
     private ScoreCalculator calculator;
 
+    /**
+     * Construct a new SlidingTileScoreManager
+     * by given a dataStream, a user, and a score calculator.
+     *
+     * @param dataStream the score dataStream
+     * @param game the current game
+     * @param user the current user
+     * @param calculator a score calculator
+     */
     public ScoreManager(ScoreDataStream dataStream, String game, String user,
                         ScoreCalculator<T> calculator){
         this.currentGame = game;
@@ -38,7 +62,7 @@ public class ScoreManager <T extends Score>{
      * @param context the context of the app
      * @return the list of scores.
      */
-    public List<T> getScoresOfGame(Context context){
+    protected List<T> getScoresOfGame(Context context){
         List<Score> scores = getGameToScore(context);
         List<T> retArray = new ArrayList<>();
         for (Score score : scores){
@@ -54,7 +78,7 @@ public class ScoreManager <T extends Score>{
      * @param context the context of the app
      * @return the list of scores.
      */
-    public List<T> getScoresOfUser(Context context){
+    protected List<T> getScoresOfUser(Context context){
         List<T> scores = getScoresOfGame(context);
         List<T> retArray = new ArrayList<>();
         for (T score: scores){
@@ -78,11 +102,13 @@ public class ScoreManager <T extends Score>{
         scores.add(score);
         dataStream.saveScores(scores, context);
     }
-    public Map getScorebyUser(String Name){
-        return null;
 
-    }
-    public Map<String, Score> getScores (){
-        return null;
+    /**
+     * get the current user that is now operating.
+     *
+     * @return the current user that is now operating
+     */
+    public String getCurrentUser() {
+        return currentUser;
     }
 }
