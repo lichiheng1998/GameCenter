b83327a7e4437620bc1f95623ea2abb4694e68e7
Wenhaoooooo <zhangwh41@gmail.com>
2018-11-26 17:34:07 -0500

    adapter of score board

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Controllers/SlidingTileScoreBoardAdapter.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Controllers/SlidingTileScoreBoardAdapter.java
@@ -1,39 +1,64 @@
 package fall2018.csc207project.SlidingTile.Controllers;
-
+import java.util.List;
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import java.util.List;
-import fall2018.csc207project.SlidingTile.Models.SlidingTileScoreManager;
+import android.widget.TextView;
+
+import fall2018.csc207project.R;
+import fall2018.csc207project.SlidingTile.Models.TileScore;
 
 public class SlidingTileScoreBoardAdapter extends BaseAdapter {
 
-    private List<SlidingTileScoreManager> slidingTileTopScores;
-    private Context context;
+    private List<TileScore> slidingTileTopScores;
+    private Context mContext;
+    private LayoutInflater mLayoutInflater = null;
 
-    SlidingTileScoreBoardAdapter(List<SlidingTileScoreManager> slidingTileTopScores, Context context) {
-        this.slidingTileTopScores = slidingTileTopScores;
-        this.context = context;
+
+    public SlidingTileScoreBoardAdapter(Context context, List<TileScore> list) {
+        mContext = context;
+        slidingTileTopScores = list;
+        mLayoutInflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
-
     @Override
     public int getCount() {
         return slidingTileTopScores.size();
     }
-
     @Override
-    public Object getItem(int position) {
-        return new String[] {slidingTileTopScores.get(position).getCurrentUser(), null, null};
+    public Object getItem(int pos) {
+        return slidingTileTopScores.get(pos);
     }
-
     @Override
     public long getItemId(int position) {
         return position;
     }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View v = convertView;
+        CompleteListViewHolder viewHolder;
+        if (convertView == null) {
+            LayoutInflater li = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = li.inflate(R.layout.tile_game_score_board_row, null);
+            viewHolder = new CompleteListViewHolder(v);
+            v.setTag(viewHolder);
+        } else {
+            viewHolder = (CompleteListViewHolder) v.getTag();
+        }
+        viewHolder.userName.setText(slidingTileTopScores.get(position).user);
+        viewHolder.userScore.setText(slidingTileTopScores.get(position).value);
+        return v;
     }
 }
+class CompleteListViewHolder {
+    public TextView userName;
+    public TextView userScore;
+    public CompleteListViewHolder(View base) {
+//        userName = (TextView) base.findViewById(R.id.user);
+    }
+}
+
