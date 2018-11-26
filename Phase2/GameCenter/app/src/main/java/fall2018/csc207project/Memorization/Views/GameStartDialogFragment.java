dd3b1ea60a5fa31f979300c82a4ba3a7374f51c3
Coco <wenjie.lu@mail.utoronto.ca>
2018-11-26 15:39:35 -0500

    Instruction Dialog class

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/Views/GameStartDialogFragment.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/Memorization/Views/GameStartDialogFragment.java
@@ -1,44 +1,27 @@
 package fall2018.csc207project.Memorization.Views;
 
-import android.content.Context;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
 import fall2018.csc207project.R;
 
 public class GameStartDialogFragment extends DialogFragment {
-    private Button proceed;
-    private TextView instructions;
-    GameStartDialogListener listener;
 
-    public interface GameStartDialogListener{
-        void proceedToGame();
-    }
-
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.memo_start_dialog, container);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        this.getDialog().setCanceledOnTouchOutside(false);
-        proceed = view.findViewById(R.id.proceed);
-
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.memo_instructions);
+        builder.setPositiveButton(R.string.memoproceed, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        return builder.create();
     }
 }
