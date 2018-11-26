ea4a5face3d930c7e80b292556b03b36a7260e6a
Arthur <arthurqin123@hotmail.com>
2018-11-25 23:16:08 -0500

    edit method

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileScore.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207project/SlidingTile/Models/TileScore.java
@@ -10,41 +10,25 @@ public class TileScore extends Score{
     public int complexity;
 
     /**
-     * name of current game
-     */
-    public final String game = "SlidingTilesGame";
-
-    /**
-     * name of the current player
-     */
-    public String user;
-
-    /**
-     * score of the specific game for a specific player in a specific complexity
-     */
-    public int value;
-
-    /**
      * number of undoSteps of the specific game for a specific player in a specific complexity
      */
     public int undoSteps;
 
     /**
-     * number of moveSteps executed for the specific game for a specific player in a specific complexity
+     * number of moveSteps executed for the specific game
+     * for a specific player in a specific complexity
      */
     public int moveSteps;
 
     /**
      * Construct a new TileScore system by given
-     * the userName, complexity, undoSteps, moveSteps
+     * complexity, undoSteps, moveSteps
      *
-     * @param userName the current user's name
      * @param complexity the complexity the user chooses
      * @param undoSteps the total undo steps the user used in Tile Game
      * @param moveSteps the total movement steps the user moves in Tile Game
      */
-    public TileScore(String userName, int complexity,  int undoSteps, int moveSteps){
-        this.user = userName;
+    public TileScore(int complexity, int undoSteps, int moveSteps){
         this.complexity = complexity;
         this.undoSteps = undoSteps;
         this.moveSteps = moveSteps;
