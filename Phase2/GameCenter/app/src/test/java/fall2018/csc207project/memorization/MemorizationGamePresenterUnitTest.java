4b24c045290daeb1c2ad49443d73ad3dbd731b1f
Coco <wenjie.lu@mail.utoronto.ca>
2018-11-29 01:51:30 -0500

    Null pointer bug fixed for memo presenter unit test

--- Phase2/GameCenter/app/src/test/java/fall2018/csc207project/memorization/MemorizationGamePresenterUnitTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc207project/memorization/MemorizationGamePresenterUnitTest.java
@@ -2,6 +2,7 @@ package fall2018.csc207project.Memorization;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.view.View;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -20,16 +22,21 @@ import fall2018.csc207project.Memorization.Controllers.MemoGamePresenter;
 import fall2018.csc207project.Memorization.Models.MemoManager;
 import fall2018.csc207project.Memorization.Views.MemoGameView;
 
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class MemorizationGamePresenterUnitTest {
-    @InjectMocks
-    private MemoGamePresenter presenter;
-    @Mock
-    private Context context;
-    @Mock
-    private MemoGameView view;
 
-    private SharedPreferences shared;
+    private MemoGamePresenter presenter;
+
+    private Context context = Mockito.mock(Context.class);
+
+    private MemoGameView view = Mockito.mock(MemoGameView.class);
+
+    private SharedPreferences shared = Mockito.mock(SharedPreferences.class);
+
 
     private MemoManager manager = new MemoManager(3, 4, false);
 
@@ -37,7 +44,9 @@ public class MemorizationGamePresenterUnitTest {
 
     @Before
     public void setUp(){
-        MockitoAnnotations.initMocks(this);
+        Mockito.when(context.getSharedPreferences(anyString(), anyInt())).thenReturn(shared);
+        Mockito.when(shared.getString("currentUser", null)).thenReturn("Admin");
+        presenter = new MemoGamePresenter(view, context);
         for(int i = 0; i < 10; i++){
             manager.updateActiveTiles();
         }
